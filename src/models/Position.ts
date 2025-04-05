41aee9eadefcd058d964080c583bff64b2ab17da
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-05 16:12:32 -0700

    Update Position.ts

--- src/models/Position.ts
+++ src/models/Position.ts
@@ -1,17 +1,41 @@
 export class Position {
-    x: number;
-    y: number;
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+  private _x: number;
+  private _y: number;
 
-    samePosition(otherPosition: Position): boolean {
-        return this.x === otherPosition.x &&
-            this.y === otherPosition.y;
-    }
+  constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
 
-    clone(): Position {
-        return new Position(this.x, this.y);
-    }
+  get x(): number {
+    return this._x;
+  }
+
+  get y(): number {
+    return this._y;
+  }
+
+  samePosition(otherPosition: Position): boolean {
+    return this._x === otherPosition._x && this._y === otherPosition._y;
+  }
+
+  equals(other: Position): boolean {
+    return this.samePosition(other);
+  }
+
+  clone(): Position {
+    return new Position(this._x, this._y);
+  }
+
+  toString(): string {
+    return `${this._x},${this._y}`;
+  }
+
+  isValid(): boolean {
+    return this._x >= 0 && this._x <= 7 && this._y >= 0 && this._y <= 7;
+  }
+
+  distanceTo(other: Position): number {
+    return Math.sqrt((this._x - other._x) ** 2 + (this._y - other._y) ** 2);
+  }
 }
